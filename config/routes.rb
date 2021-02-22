cb77c7e2cd04ea91d75801f99d47585a4dd6c288
Owen S <owen.skilton@gmail.com>
2021-02-22 12:55:57 +0000

    Devise controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'users/registrations', :confirmations => 'users/confirmations', 
+                                       :passwords => 'users/passwords', :sessions => 'users/sessions',:unlocks => 'users/unlocks'}
   resources :posts
   get 'home/about'
   root 'home#index'
